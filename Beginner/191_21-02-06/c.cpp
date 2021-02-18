b3fe7d59c52a5b91768b7f18666a08c010b1f045
Shiro-yutani <dy.yutani@gmail.com>
2021-02-18 22:34:00 +0900

    Solved ABC-191-C

--- Beginner/191_21-02-06/c.cpp
+++ Beginner/191_21-02-06/c.cpp
@@ -5,23 +5,22 @@ using namespace std;
 
 int main(){
     int h,w,count, peak=0;
-    string s[6];
+    string s[10];
 
     cin >> h >> w;
     for(int i=0; i<h; i++){
         cin >> s[i];
     }
 
-    for(int i=1; i<h-1; i++){
-        for(int j=1; j<w-1; j++){
-            if(s[i][j]=='#'){
-                count=0;
-                if(s[i][j-1] == '.'){ count++;}
-                if(s[i][j+1] == '.'){ count++;}
-                if(s[i-1][j] == '.'){ count++;}
-                if(s[i+1][j] == '.'){ count++;}
-            }
-            if(count>2) peak++;
+    for(int i=1; i<h; i++){
+        for(int j=1; j<w; j++){
+            count=0;
+            if(s[i-1][j-1] == '#'){ count++;}
+            if(s[i-1][j] == '#'){ count++;}
+            if(s[i][j-1] == '#'){ count++;}
+            if(s[i][j] == '#'){ count++;}
+            if(count%2) peak++;
+//            cout <<__LINE__<< s[i-1][j-1] << s[i-1][j] << s[i][j-1] << s[i][j] << ", count=" << count << ", peak=" << peak << endl;
         }
     }
     cout << peak;
